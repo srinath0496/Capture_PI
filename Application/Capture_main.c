85a01e2cca25e1bc39e4e48934c9d582a7c19f67
srinath0496 <srinath.adipudi@gmail.com>
2017-06-07 02:12:24 +0530

    Working Copy

--- Application/Capture_main.c
+++ Application/Capture_main.c
@@ -3,6 +3,7 @@
 #include<string.h>
 #include "bcm2835.h"
 #include "typedef.h"
+#include "Capture_main.h"
 void main()
 {
     UINT_8 recv_buf;
@@ -15,6 +16,10 @@ void main()
     }
     else
     {
+        ads1298_gpio_init();
+        spi_init();
+        ads1298_init();
+
         while(1)
         {
             printf("Press any key to write\n");
@@ -22,13 +27,24 @@ void main()
             //spi_select(CS1);
             //spi_rdwr();
             //spi_deselect(CS1);
-            recv_buf = bcm2835_spi_transfer(0x00);
+            bcm2835_gpio_clr(RPI_GPIO_P1_22); //CS1
+            bcm2835_spi_transfer(0x20);
+            //printf("The Receive value is %X\n",recv_buf);
+            bcm2835_delayMicroseconds(5);
+            recv_buf =  bcm2835_spi_transfer(0x00);
+            bcm2835_delayMicroseconds(5);
+            recv_buf =  bcm2835_spi_transfer(0x00);
             printf("The Receive value is %X\n",recv_buf);
+            bcm2835_delayMicroseconds(1);
+            bcm2835_gpio_set(RPI_GPIO_P1_22); //CS1
+        }
     }
 }
 
 UINT_8 bcm_driver_init()
 {
+    UINT_8 t,w;
+    int i;
     if(!bcm2835_init())
     {
         printf("GPIO driver not Initialise\n");
@@ -37,22 +53,60 @@ UINT_8 bcm_driver_init()
     else
     {
         printf("Driver Initialise\n");
-        if(!bcm2835_spi_begin())
-        {
-            printf("The SPI device failed to start\n");
-            return 1;
-        }
-        else
-        {
-            printf("The SPI device started\n");
-            printf("Configuring the SPI interface");
-            bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
-            bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);
-            bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_2048);
-            bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
-            bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0,LOW);
+        bcm2835_gpio_fsel(RPI_GPIO_P1_18, BCM2835_GPIO_FSEL_OUTP); //SPI_START
+        bcm2835_gpio_fsel(RPI_GPIO_P1_15, BCM2835_GPIO_FSEL_OUTP); //PIN reset
+        bcm2835_gpio_fsel(RPI_GPIO_P1_22, BCM2835_GPIO_FSEL_OUTP); //CS1
+        bcm2835_gpio_clr(RPI_GPIO_P1_18);  //SPI_START
+        bcm2835_gpio_clr(RPI_GPIO_P1_15); // PIN RESET
+        bcm2835_gpio_set(RPI_GPIO_P1_22); //CS1
+        bcm2835_delay(5);
+        printf("Configuration Completed\n");
 
-        }
     }
-    //return 0;
+    return 0;
+}
+
+UINT_8 spi_init()
+{
+    if(!bcm2835_spi_begin())
+    {
+        printf("The SPI device failed to start\n");
+        return 1;
+    }
+    else
+    {
+        printf("The SPI device started\n");
+        printf("Configuring the SPI interface");
+        bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+        bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);
+        bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_512);
+        //bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+        //bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0,LOW);
+        bcm2835_delay(1);
+    }
+    return 0;
+}
+
+UINT_8 ads1298_gpio_init()
+{
+    bcm2835_gpio_set(RPI_GPIO_P1_15); //Set the RESET Pin to HIGH
+    bcm2835_delay(1000);
+    bcm2835_gpio_clr(RPI_GPIO_P1_15); //Set the RESET Pin to LOW
+    bcm2835_delay(1000);
+    bcm2835_gpio_set(RPI_GPIO_P1_15); //Set the RESET Pin to HIGH
+    bcm2835_delay(100);
+    bcm2835_gpio_clr(RPI_GPIO_P1_22); //CS1
+    bcm2835_delay(1000);
+    bcm2835_gpio_set(RPI_GPIO_P1_22); //CS1
+    return 0;
+}
+
+UINT_8 ads1298_init()
+{
+    bcm2835_gpio_clr(RPI_GPIO_P1_22); //CS1
+    bcm2835_spi_transfer(0x11);
+    bcm2835_delay(10);
+    bcm2835_gpio_set(RPI_GPIO_P1_22); //CS1
+    //printf("The read ID is %X\n",bcm2835_spi_transfer(0x20));
+    return 0;
 }
--- Includes/Capture_main.h
+++ Includes/Capture_main.h
@@ -4,4 +4,7 @@ UINT_8 bcm_driver_init();
 UINT_8 spi_select(UINT_8 chip_sel);
 UINT_8 spi_deselect(UINT_8 chip_sel);
 UINT_8 spi_rdwr();
+UINT_8 spi_init();
+UINT_8 ads1298_init();
+UINT_8 ads1298_gpio_init();
 //UINT_8 spi_read(UINT_8* data);
